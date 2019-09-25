8aa8d4fb2650a044388e1a45e8eb901e8a755555
Kirill Zhurin <kirillzhurin@gmail.com>
2019-09-25 21:14:23 +0300

    added creation lights

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './index.scss';
+import Scene from './Scene';
+import createLights from './lights';
+
+window.addEventListener('load', () => {
+  const container = document.getElementById('world');
+  // set up the scene, the camera and the renderer
+  const scene = new Scene({ container });
+  
+  // add the lights
+  createLights(scene);
+
+  
+});
